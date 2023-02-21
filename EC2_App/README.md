7c606b7e22927b66f3bc9afeef5c6535c6d38d1e
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-02-21 09:59:36 +0300

    -

--- EC2_App/README.md
+++ EC2_App/README.md
@@ -1,2 +1,6 @@
-# deploy app using git clone (wait til it finsh)
+# What does it do 
+deploy app using git clone
+
+# hat does it clone?
+<code>https://github.com/Khald1998/Clone.git</code> <br>
 
--- EC2_App/user_data.sh
+++ EC2_App/user_data.sh
@@ -4,7 +4,7 @@ sudo apt install git -y
 wget https://storage.googleapis.com/golang/go1.19.5.linux-amd64.tar.gz /home/ubuntu
 sudo tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
 echo "PATH=$PATH:/usr/local/go/bin" >> /etc/environment
-git clone https://github.com/Khald1998/My_REST_API_TEMP.git /home/ubuntu/golangapp
+git clone https://github.com/Khald1998/Clone.git /home/ubuntu/golangapp
 chown ubuntu:ubuntu /home/ubuntu/golangapp
 sudo chmod -R 777 /home
 git config --global --add safe.directory /home/ubuntu/golangapp
