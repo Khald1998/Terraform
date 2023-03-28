2fda76dc453f84b61ac17f902a7e7b278ee7876b
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-03-28 15:35:11 +0300

    -

--- GCP/Compute_Engine_App(docker)/IAM.tf
+++ GCP/Compute_Engine_App(docker)/IAM.tf
@@ -1,34 +1,34 @@
-# resource "google_project_iam_binding" "Service_Account_Token_Creator" {
-#   project = var.gcp_project
-#   role    = "roles/iam.serviceAccountTokenCreator"
-#   members = [
-#   "user:${var.user_email}"
-#   ,"serviceAccount:${google_service_account.registry_access.email}"
-#   ]
-#   depends_on = [
-#     google_service_account.registry_access
-#   ]
-# }
+resource "google_project_iam_binding" "Service_Account_Token_Creator" {
+  project = var.gcp_project
+  role    = "roles/iam.serviceAccountTokenCreator"
+  members = [
+  "user:${var.user_email}"
+  ,"serviceAccount:${google_service_account.registry_access.email}"
+  ]
+  depends_on = [
+    google_service_account.registry_access
+  ]
+}
 
 
-# resource "google_storage_bucket_iam_member" "viewer" {
-#   bucket = "artifacts.terraform-31308.appspot.com"
-#   role   = "roles/storage.objectViewer"
-#   member = "serviceAccount:${google_service_account.registry_access.email}"
-#   depends_on = [
-#     google_service_account.registry_access
-#   ]
+resource "google_storage_bucket_iam_member" "viewer" {
+  bucket = "artifacts.terraform-31308.appspot.com"
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_service_account.registry_access.email}"
+  depends_on = [
+    google_service_account.registry_access
+  ]
 
-# }
+}
 
-# resource "google_storage_bucket_iam_member" "Admin" {
-#   bucket = "artifacts.terraform-31308.appspot.com"
-#   role   = "roles/storage.objectAdmin"
-#   member = "serviceAccount:${google_service_account.registry_access.email}"
-#   depends_on = [
-#     google_service_account.registry_access
-#   ]
-# }
+resource "google_storage_bucket_iam_member" "Admin" {
+  bucket = "artifacts.terraform-31308.appspot.com"
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.registry_access.email}"
+  depends_on = [
+    google_service_account.registry_access
+  ]
+}
 
 
 resource "google_project_iam_member" "viewer" {
