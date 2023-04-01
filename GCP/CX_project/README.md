4e4687d28ec46c72db0dfeb2edcaa01268ef1dc0
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-04-01 16:03:20 +0300

    -

--- GCP/CX_project/README.md
+++ GCP/CX_project/README.md
@@ -1,3 +1,4 @@
 cd app; npm init -y; 
 npm install express
+npm install express graphql express-graphql
 node server.js
--- GCP/CX_project/app/server.js
+++ GCP/CX_project/app/server.js
@@ -1,11 +1,41 @@
+// const express = require('express');
+// const app = express();
+// const port = 8080;
+
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
+
+
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Define a schema
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// Define a resolver
+const root = {
+  hello: () => 'Hello world!'
+};
+
+// Create an express server and add the graphql middleware
 const app = express();
-const port = 8080;
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true // This enables the GraphiQL UI for testing and exploring the API
+}));
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+// Start the server
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000/graphql');
 });
