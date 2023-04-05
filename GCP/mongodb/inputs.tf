9406ac19b5bebdcbe9df715b4c54be77a0e862dc
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-04-05 17:31:57 +0300

    -

---
+++ GCP/mongodb/inputs.tf
@@ -0,0 +1,12 @@
+variable "gcp_region" {
+  type        = string
+  description = "Region to use for GCP provider"
+  default     = "us-central1"
+}
+
+variable "gcp_project" {
+  type        = string
+  description = "Project to use for this config"
+  default     = "terraform-31308"
+}
+
