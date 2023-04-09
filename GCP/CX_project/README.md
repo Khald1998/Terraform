9fe4061602325b3e25b0caf0f76797eece9b058b
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-04-09 12:32:11 +0300

    -

--- GCP/CX_project/README.md
+++ GCP/CX_project/README.md
@@ -1,4 +1,44 @@
-cd app; npm init -y; 
+cd app; npm init -y;
 npm install express
 npm install express graphql express-graphql
 node server.js
+
+query {
+hello
+}
+<br>
+{
+getUser(id: 1) {
+id
+name
+email
+}
+}
+<br>
+{
+getUsers {
+id
+name
+email
+}
+}
+<br>
+mutation {
+addUser(name: "Bob Smith", email: "bobsmith@example.com") {
+id
+name
+email
+}
+}
+<br>
+mutation {
+deleteUser(id: 1)
+}
+<br>
+mutation {
+updateUser(id: 1, name: "John Smith", email: "johnsmith@example.com") {
+id
+name
+email
+}
+}
--- GCP/CX_project/app/server.js
+++ GCP/CX_project/app/server.js
@@ -1,41 +1,86 @@
-// const express = require('express');
-// const app = express();
-// const port = 8080;
-
-// app.get('/', (req, res) => {
-//     res.send('Hello, World!');
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server listening on port ${port}`);
-// });
-
-
+// Import necessary packages
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const port = 8080;
 
-// Define a schema
+// Define schema
 const schema = buildSchema(`
   type Query {
     hello: String
+    getUser(id: Int!): User
+    getUsers: [User]
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!): User
+    deleteUser(id: Int!): Boolean
+    updateUser(id: Int!, name: String, email: String): User
+  }
+
+  type User {
+    id: Int
+    name: String
+    email: String
   }
 `);
 
-// Define a resolver
-const root = {
-  hello: () => 'Hello world!'
+// Define some mock data for testing purposes
+const users = [
+  { id: 1, name: 'John Doe', email: 'johndoe@example.com' },
+  { id: 2, name: 'Jane Smith', email: 'janesmith@example.com' },
+];
+
+// Define root resolver
+const rootResolver = {
+  hello: () => {
+    return 'Hello, world!';
+  },
+  getUser: ({ id }) => {
+    return users.find(user => user.id === id);
+  },
+  getUsers: () => {
+    return users;
+  },
+  addUser: ({ name, email }) => {
+    const id = users.length + 1;
+    const newUser = { id, name, email };
+    users.push(newUser);
+    return newUser;
+  },
+  deleteUser: ({ id }) => {
+    const index = users.findIndex(user => user.id === id);
+    if (index === -1) {
+      return false;
+    }
+    users.splice(index, 1);
+    return true;
+  },
+  updateUser: ({ id, name, email }) => {
+    const index = users.findIndex(user => user.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const updatedUser = { ...users[index], name, email };
+    users[index] = updatedUser;
+    return updatedUser;
+  },
 };
 
-// Create an express server and add the graphql middleware
+// Create an Express app
 const app = express();
+
+// Add GraphQL endpoint
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
-  graphiql: true // This enables the GraphiQL UI for testing and exploring the API
+  rootValue: rootResolver,
+  graphiql: true, // enable GraphiQL tool for debugging/testing
 }));
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000/graphql');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
